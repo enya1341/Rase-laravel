d2fe1ed7f16259c4316e5ca387f9aaf9876c23dc
enya <ryuusei.y@icloud.com>
2021-07-18 10:18:27 +0900

    readmeの追加

--- Rase-lara/README.md
+++ Rase-lara/README.md
@@ -1,3 +1,144 @@
-# 飲食店予約サービス
+# Rase-lara (バックエンド)
 
-開発test
+<br>
+
+
+## ※フロントエンド部分に大方のことは書いたのでこちらは基本情報とバックエンド部分のapiとシーディングデータについてのみ記載
+
+<br>
+
+> ### 使用言語
+
+<br>
+
+- Laravel
+
+  - PHP
+
+<br>
+
+> ### サーバー
+
+<br>
+
+- heroku [Rase-lara](https://dashboard.heroku.com/apps/whispering-retreat-97645)
+
+<br>
+
+> ### API
+
+<br>
+
+- 会員登録
+  - Post
+  - api/v1/users/registration
+
+<br>
+
+- ログイン
+  - Post
+  - api/v1/users/login
+- ログアウト
+  - Post
+  - api/v1/users/logout
+
+<br>
+
+- ユーザー取得
+  - Get
+  - api/v1/users
+
+<br>
+
+- お気に入り情報取得
+  - Get
+  - api/v1/{user_id}/favorites
+- 予約情報取得
+  - Get
+  - api/v1/{user_id}/reservations
+
+<br>
+
+- ストア一覧取得
+  - Get
+  - api/v1/stores
+- ストア詳細取得
+  - Get
+  - api/v1/{store_id}/stores
+
+<br>
+
+- お気に入り追加
+  - Post
+  - api/v1/{user_id}/favorites
+- お気に入り削除
+  - Delete
+  - api/v1/{user_id}/favorites
+
+<br>
+
+- 評価
+  - Post
+  - api/v1/{store_id}/values
+- 評価情報取得
+  - Get
+  - api/v1/{store_id}/values
+
+<br>
+
+---管理者権限---
+
+<br>
+
+- 全ユーザー情報取得
+  - Get
+  - api/v1/{email}/users
+- 店舗代表者権限付与
+  - Put
+  - api/v1/storeAdminGrant/users
+- 店舗代表者権限削除
+  - Put
+  - api/v1/storeAdminDelete/users
+
+<br>
+
+---店舗代表者権限---
+
+<br>
+
+- 店舗情報作成
+  - Post
+  - api/v1/storeAdmin/stores
+- 店舗情報更新
+  - Put
+  - api/v1/{store_id}/storeAdmin/stores
+- 店舗画像をS3に送りパスをDBに保存する
+  - Post
+  - api/v1/{store_id}/storeAdmin/stores/image
+- 店舗の予約情報取得
+  - Get
+  - api/v1/{store_id}/storeAdmin/reservations
+
+<br>
+
+> ### シーディング
+
+<br> 
+
+- Store
+
+  - 二十件分のストアデータのシーディング
+
+<br> 
+
+- User
+
+  - 管理者権限と店舗代表者権限を持ったアカウントのシーディング
+
+
+
+
+
+
+
+
--- Rase-lara/routes/api.php
+++ Rase-lara/routes/api.php
@@ -10,44 +10,62 @@ use App\Http\Controllers\StoresController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\ValuesController;
 
-
+// 会員登録のapi
 Route::post('/v1/users/registration', [RegisterController::class, 'post']);
 
-
+// ログインとログアウトのapiは現状使ってませんが一応残してます
+// ログインのapi
 Route::post('/v1/users/login', [LoginController::class, 'post']);
+// ログアウトのapi
 Route::post('/v1/users/logout', [LogoutController::class, 'post']);
 
-
+//ログイン時のユーザー取得api
 Route::get('/v1/users', [UsersController::class, 'get']);
 
+//お気に入り情報取得のapi
 Route::get('/v1/{user_id}/favorites', [FavoritesController::class, 'get']);
+//予約情報取得のapi
 Route::get('/v1/{user_id}/reservations', [ReservationsController::class, 'get']);
 
-
+//ストア一覧の取得api
 Route::get('/v1/stores', [StoresController::class, 'storeget'])->name('storeget');
+//ストア詳細の取得api
 Route::get('/v1/{store_id}/stores', [StoresController::class, 'storedata'])->name('storedata');
 
-
+//お気に入り追加api
 Route::post('/v1/{user_id}/favorites', [FavoritesController::class, 'post']);
+//お気に入り削除api
 Route::delete('/v1/{user_id}/favorites', [FavoritesController::class, 'delete']);
 
+//予約追加api
 Route::post('/v1/{store_id}/reservations', [ReservationsController::class, 'post']);
+//予約変更api
 Route::put('/v1/{store_id}/reservations', [ReservationsController::class, 'put']);
+//予約削除api
 Route::delete('/v1/{user_id}/reservations', [ReservationsController::class, 'delete']);
 
+//評価追加api
 Route::post('/v1/{store_id}/values', [ValuesController::class, 'post']);
+//評価情報取得api
 Route::get('/v1/{store_id}/values', [ValuesController::class, 'get']);
 
 
 // 管理者権限
 
+//全ユーザー情報取得api
 Route::get('/v1/{email}/users', [UsersController::class, 'adminuser'])->name('adminuser');
+//店舗代表者権限付与api
 Route::put('/v1/storeAdminGrant/users', [UsersController::class, 'storeAdminGrant'])->name('storeAdminGrant');
+//店舗代表者権限削除api
 Route::put('/v1/storeAdminDelete/users', [UsersController::class, 'storeAdminDelete'])->name('storeAdminDelete');
 
 // 店舗代表者権限
 
+//店舗情報作成api
 Route::post('/v1/storeAdmin/stores', [StoresController::class, 'post']);
-Route::post('/v1/{store_id}/storeAdmin/stores/image', [StoresController::class, 'storeImagePost'])->name('storeImagePost');;
+//店舗情報更新api
 Route::put('/v1/{store_id}/storeAdmin/stores', [StoresController::class, 'put']);
+//店舗画像をS3に送りパスをDBに保存するapi
+Route::post('/v1/{store_id}/storeAdmin/stores/image', [StoresController::class, 'storeImagePost'])->name('storeImagePost');
+//店舗の予約情報取得api
 Route::get('/v1/{store_id}/storeAdmin/reservations', [ReservationsController::class, 'storeAdminReservations'])->name('storeAdminReservations');
